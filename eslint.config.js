f836ca825a697485fda3b84683266fb0b63565ed
kasra torabi <Kasra.torabiii@gmail.com>
2025-03-29 13:07:40 +0330

    build-v1

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
