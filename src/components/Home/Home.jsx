3d709d2456079a6a329ce9d60b35e22690187e34
kasra torabi <Kasra.torabiii@gmail.com>
2025-04-01 23:42:12 +0330

    firstday

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css'
+
+import Header from '../Header/Header'
+
+const home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default home
